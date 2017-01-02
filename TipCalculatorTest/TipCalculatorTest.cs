3288e6060d5cd44d4626fa245486b70349920a2a
jame2408 <jame2408@gmail.com>
2017-01-02 16:07:58 +0800

    Second：新增四捨五入的測試個案(紅燈)

--- TipCalculatorTest/TipCalculatorTest.cs
+++ TipCalculatorTest/TipCalculatorTest.cs
@@ -5,6 +5,11 @@ namespace TipCalculatorTest
     [TestClass]
     public class TipCalculatorTest
     {
+        TipCalculator.TipCalculator target;
+        public TipCalculatorTest()
+        {
+            target = new TipCalculator.TipCalculator();
+        }
         [TestMethod]
         public void 輸入10元_小費15啪_需給額外小費1點50元_總共要給11點50元()
         {
@@ -13,12 +18,26 @@ namespace TipCalculatorTest
             double tipRate = 15;
 
             //Act
-            var target = new TipCalculator.TipCalculator();
             target.CaclTipAndTotalAmount(billAmount, tipRate);
 
             //Assert
             Assert.AreEqual(1.50d, target.tip);
             Assert.AreEqual(11.50d, target.totalAmount);
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            //arrange
+            double billAmount = 11.25d;
+            double tipRate = 15;
+
+            //Act
+            target.CaclTipAndTotalAmount(billAmount, tipRate);
+
+            //Assert
+            Assert.AreEqual(1.69d, target.tip);
+            Assert.AreEqual(12.94d, target.totalAmount);
         }
     }
 }
